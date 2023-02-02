78e4f657174c32015e5f7f0b433085f048e1977a
Sárándi <sarandi.balint@gmail.com>
2023-02-02 20:16:41 +0100

    ResponseEntity
    Http status

--- src/main/java/com/example/pizzaproject/user/UserController.java
+++ src/main/java/com/example/pizzaproject/user/UserController.java
@@ -1,6 +1,8 @@
 package com.example.pizzaproject.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,17 +26,19 @@ public class UserController {
     }
 
     @PostMapping(path = "/register")
-    public void registerNewStudent(@RequestBody User user) {
+    public ResponseEntity<String> registerNewStudent(@RequestBody User user) {
         userService.addNewUser(user);
+        return new ResponseEntity<>("User created successfully", HttpStatus.OK);
     }
 
     @DeleteMapping(path = "{userId}")
-    public void deleteUser(@PathVariable("userId") Long userId) {
+    public ResponseEntity<String> deleteUser(@PathVariable("userId") Long userId) {
         userService.deleteUser(userId);
+        return new ResponseEntity<>("User deleted successfully", HttpStatus.OK);
     }
 
     @PutMapping(path = "{userId}")
-    public void updateUser(
+    public ResponseEntity<String> updateUser(
             @PathVariable("userId") Long studentId,
             @RequestParam(required = false) String first_name,
             @RequestParam(required = false) String last_name,
@@ -43,5 +47,6 @@ public class UserController {
             @RequestParam(name = "admin", required = false) boolean admin,
             @RequestParam(name = "cook", required = false) boolean cook){
         userService.updateUser(studentId, first_name, last_name, email, password, admin, cook);
+        return new ResponseEntity<>("User updated successfully", HttpStatus.OK);
     }
 }
