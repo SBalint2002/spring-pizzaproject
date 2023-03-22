3f8643ab3c3c729dc70976dfd7a07270a64fabbc
Sárándi <sarandi.balint@gmail.com>
2023-03-22 12:59:24 +0100

    Fixing code, lombok
    JwtResponse now contains: accessToken and refreshToken
    ResponseEntity returns for making each one similar.
    Lombok to make better class
    Role enum instead of isAdmin boolean

--- src/main/java/com/example/pizzaproject/auth/JwtResponse.java
+++ src/main/java/com/example/pizzaproject/auth/JwtResponse.java
@@ -1,37 +1,14 @@
 package com.example.pizzaproject.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class JwtResponse {
-    private String JWTToken;
-    private String status;
-    private String RefreshToken;
+    private String accessToken;
+    private String refreshToken;
 
-    public JwtResponse(String status, String JWTToken, String RefreshToken) {
-        this.status = status;
-        this.JWTToken = JWTToken;
-        this.RefreshToken = RefreshToken;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getJWTToken() {
-        return JWTToken;
-    }
-
-    public void setJWTToken(String JWTToken) {
-        this.JWTToken = JWTToken;
-    }
-
-    public String getRefreshToken() {
-        return RefreshToken;
-    }
-
-    public void setRefreshToken(String refreshToken) {
-        RefreshToken = refreshToken;
-    }
 }
---
+++ src/main/java/com/example/pizzaproject/user/Role.java
@@ -0,0 +1,6 @@
+package com.example.pizzaproject.user;
+
+public enum Role {
+    ADMIN,
+    USER
+}
