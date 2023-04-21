fb01effb5e7250e5394f0ac88ed4de9ba4498012
Zsombor Makker <makker.zsombor@gmail.com>
2023-04-21 13:40:24 +0200

    Dtos Documented

--- src/main/java/hu/pizzavalto/pizzaproject/dto/UserRegisterDto.java
+++ src/main/java/hu/pizzavalto/pizzaproject/dto/UserRegisterDto.java
@@ -6,23 +6,33 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+/**
+ * Validációval egybe kötött Felhasználó DataTransferObjekt.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRegisterDto {
-
+    /**
+     * Validálja, hogy a keresztnév min 2 maximum 50 karakter hosszú és nem üres.
+     */
     @Size(min = 2, max = 50)
     private String first_name;
+    /**
+     * Validálja, hogy a vezetéknév min 2 maximum 50 karakter hosszú és nem üres.
+     */
     @Size(min = 2, max = 50)
     private String last_name;
-
+    /**
+     * Validálja, hogy az email email típusú és nem üres.
+     */
     @Email
     @NotBlank
     private String email;
-
+    /**
+     * Validálja, hogy a jelszó min 6 maximum 255 karakter hosszú és nem üres.
+     */
     @NotBlank
     @Size(min = 6, max = 255)
     private String password;
-
 }
--- src/main/java/hu/pizzavalto/pizzaproject/dto/UserResponseDto.java
+++ src/main/java/hu/pizzavalto/pizzaproject/dto/UserResponseDto.java
@@ -4,15 +4,32 @@ import hu.pizzavalto.pizzaproject.model.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+/**
+ * Validációval egybe kötött FelhasználóVálasz DataTransferObjekt.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserResponseDto {
+    /**
+     * FelhasználóVálasz DataTransferObjekt id-je.
+     */
     private Long id;
+    /**
+     * FelhasználóVálasz DataTransferObjekt keresztneve.
+     */
     private String first_name;
+    /**
+     * FelhasználóVálasz DataTransferObjekt vezetékneve.
+     */
     private String last_name;
+    /**
+     * FelhasználóVálasz DataTransferObjekt emailje.
+     */
     private String email;
+    /**
+     * FelhasználóVálasz DataTransferObjekt szerepköre.
+     */
     private Role role;
 
 }
